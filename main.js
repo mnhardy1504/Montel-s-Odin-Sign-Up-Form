221793c67b02ca721a388d8417e29a44299986e1
“[Montel <[mhsports345@gmail.com]>
2024-04-10 23:13:23 -0400

    made button, added shadows

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirmPassword").value;
+
+    let error = document.querySelector("#error");
+
+    if(password = "" && confirmPassword == "") {
+        error.innerHTML = "*passwords do not match"
+    }
+
+    Element if(password != confirmPassword){
+        error.innerHTML = ""
+    }
+
+    console.log("hi")
+})
+
